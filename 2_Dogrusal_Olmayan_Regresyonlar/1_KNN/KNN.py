4c70ee2a2d553b4f94d41df9e6f8f6b75b3f8bc3
mmeliht <63072570+mmeliht@users.noreply.github.com>
2022-09-30 16:06:50 +0300

    Add files via upload

--- 2_Dogrusal_Olmayan_Regresyonlar/1_KNN/KNN.py
+++ 2_Dogrusal_Olmayan_Regresyonlar/1_KNN/KNN.py
@@ -1,25 +1,13 @@
 #K en yakın komşu algoritması
 
-
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split,GridSearchCV
-from sklearn.metrics import mean_squared_error,r2_score
-import matplotlib.pyplot as plt
-from sklearn.preprocessing import scale
-from sklearn.preprocessing import StandardScaler
-from sklearn import model_selection
-from sklearn.linear_model import LinearRegression
-from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import mean_squared_error
 from sklearn.neighbors import KNeighborsRegressor
-from sklearn.neural_network import MLPRegressor
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.ensemble import GradientBoostingRegressor
-from sklearn import neighbors
-from sklearn.svm import SVR
-
-
 from warnings import filterwarnings
+
+
 filterwarnings("ignore") #uyarı mesajlarını engeller
 
 #veri seti ve test seti oluşturma 
@@ -34,14 +22,12 @@ X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.25,random_state=4
 #Model oluşturma
 knn_model=KNeighborsRegressor().fit(X_train,y_train)
 
-
 #Tahmin
 y_pred=knn_model.predict(X_test,)
 np.sqrt(mean_squared_error(y_test, y_pred))#hata kareler ortalmasının karekökü
 #ilkel test hatası hesaplandı
 
 #Model Tuning
-
 RMSE=[]
 
 for k in range(10):
