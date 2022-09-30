c25db2dad6e8023e8c2a0e7ec7b810f4059ce5d6
mmeliht <63072570+mmeliht@users.noreply.github.com>
2022-09-30 16:03:45 +0300

    Add files via upload

--- 2_Dogrusal_Olmayan_Regresyonlar/1_KNN/KNN.py
+++ 2_Dogrusal_Olmayan_Regresyonlar/1_KNN/KNN.py
@@ -18,8 +18,8 @@ from sklearn.ensemble import GradientBoostingRegressor
 from sklearn import neighbors
 from sklearn.svm import SVR
 
-from warnings import filterwarnings
 
+from warnings import filterwarnings
 filterwarnings("ignore") #uyarı mesajlarını engeller
 
 #veri seti ve test seti oluşturma 
@@ -32,3 +32,101 @@ X=pd.concat([X_,dms[["League_N","Division_W","NewLeague_N"]]],axis=1)
 X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.25,random_state=42)
 
 #Model oluşturma
+knn_model=KNeighborsRegressor().fit(X_train,y_train)
+
+
+#Tahmin
+y_pred=knn_model.predict(X_test,)
+np.sqrt(mean_squared_error(y_test, y_pred))#hata kareler ortalmasının karekökü
+#ilkel test hatası hesaplandı
+
+#Model Tuning
+
+RMSE=[]
+
+for k in range(10):
+    k+=1
+    knn_model=KNeighborsRegressor(n_neighbors=k).fit(X_train,y_train)
+    p_pred=knn_model.predict(X_test)
+    rmse=np.sqrt(mean_squared_error(y_test, y_pred))
+    RMSE.append(rmse)
+   # print("k: ",k,"için RMSE degeri: ",rmse)
+
+#GridSearchCV
+#Belirlemeye çalıştığımız hiper parametrelerin değerlerini belirlemeke için kullanılan fonksiyon
+#Birbirinden farklı ve fazla parametre olduğunda kullanımı avantajlıdır.
+knn_params={"n_neighbors":np.arange(1,30,1)}
+knn=KNeighborsRegressor()
+knn_cv_model=GridSearchCV(knn, knn_params, cv=10).fit(X_train, y_train)
+knn_cv_model.best_params_
+
+#Final Model
+knn_tuned=KNeighborsRegressor(n_neighbors=knn_cv_model.best_params_["n_neighbors"]).fit(X_train,y_train)
+
+#Final Model Test Hatası
+y_pred=knn_tuned.predict(X_test)
+np.sqrt(mean_squared_error(y_test, y_pred))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
